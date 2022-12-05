0823714bc497dc807b18ad08ea2e7442c28da850
bahnisikha <bahnisikhadhar@gmail.com>
2022-12-05 13:52:01 +0530

    added local storage facility and also modified the search bar

--- index.js
+++ index.js
@@ -1,54 +1,58 @@
-const search=document.querySelector(".search");
-const searchPaan=document.querySelector(".search_paan");
-const sequenceSearch=document.querySelector(".sequence");
-const errorPaan=document.querySelector(".error_paan");
-const formInputDetail=document.querySelector(".input_detail");
-const inputName=document.querySelector(".input_name");
-const inputCity=document.querySelector(".input_city");
-const inputPaan=document.querySelector(".input_paan");
-const inputMail=document.querySelector(".input_mail");
-const addButton=document.querySelector(".add_btn");
-const errorInput=document.querySelector(".error_input");
-const outputList=document.querySelector(".output_list");
-const searchButton=document.querySelector(".search_btn");
-const crossIcon=document.querySelector(".fa-xmark")
+const search = document.querySelector(".search");
+const searchPaan = document.querySelector(".search_paan");
+const sequenceSearch = document.querySelector(".sequence");
+const errorPaan = document.querySelector(".error_paan");
+const formInputDetail = document.querySelector(".input_detail");
+const inputName = document.querySelector(".input_name");
+const inputCity = document.querySelector(".input_city");
+const inputPaan = document.querySelector(".input_paan");
+const inputMail = document.querySelector(".input_mail");
+const addButton = document.querySelector(".add_btn");
+const errorInput = document.querySelector(".error_input");
+const outputList = document.querySelector(".output_list");
+const searchButton = document.querySelector(".search_btn");
+const crossIcon = document.querySelector(".fa-xmark")
 
-inputDetailArr=JSON.parse(localStorage.getItem("detail")) || [];
-inputDetailArr=[];
-let count=0;
+inputDetailArr = JSON.parse(localStorage.getItem("details")) || [];
+// inputDetailArr=[];
+let count = 0;
 let listName, listMail, listPaan, listCity;
+//--------------------------------------DISPLAY FROM LOCAL STORAGE EVEN AFTER REFRESHING--------------------------------
+
+inputDetailArr.map((ele)=>{
+   createListElement(ele);
+})
 
 //----------------------------------------TO RESET INPUT AND ERROR FIELD---------------------------------
 
 function reset()
 {
-   inputName.value="";
-   inputCity.value="";
-   inputPaan.value="";
-   inputMail.value="";
-   errorPaan.innerText="";
-   errorInput.innerText="";
+   inputName.value = "";
+   inputCity.value = "";
+   inputPaan.value = "";
+   inputMail.value = "";
+   errorPaan.innerText = "";
+   errorInput.innerText = "";
    
 }
 //-------------------------------CREATE LIST ELEMENT------------------------------------------------------------
 
 function createListElement(inputDetailObj)
 {
-   const listEle=document.createElement("li");
+   const listEle = document.createElement("li");
    listEle.setAttribute("id",inputDetailObj.id);
    listEle.setAttribute("class","li_decoration");
-   listEle.innerHTML= `<div class="output_decor">${inputDetailObj.name}</div>
-   <div class="output_decor">${inputDetailObj.city}</div>
-   <div class="output_decor">${inputDetailObj.paan}</div>
-   <div class="output_decor">${inputDetailObj.mail}</div>
-   <button class="edit_btn">edit</button>
-   <button class="delete_btn">delete</button>`;
+   listEle.innerHTML = `<div class="output_decor">${inputDetailObj.name}</div>
+                        <div class="output_decor">${inputDetailObj.city}</div>
+                        <div class="output_decor">${inputDetailObj.paan}</div>
+                        <div class="output_decor">${inputDetailObj.mail}</div>
+                        <button class="edit_btn">edit</button>
+                        <button class="delete_btn">delete</button>`;
    outputList.appendChild(listEle);
    
 }
 
 //--------------------------------ADD OBJECT-ARRAY & SHOW ERROR MESSAGE-------------------------------------- 
-
 
 function addInput(event){
     event.preventDefault();
@@ -104,14 +108,14 @@ function addInput(event){
        };
        inputDetailArr.push(inputDetailObj);
 
-       localStorage.setItem("detail",JSON.stringify(inputDetailArr));
+       localStorage.setItem("details",JSON.stringify(inputDetailArr));
 
        createListElement(inputDetailObj);
        reset();
     }
     }
     }
-//------------------------------------------TO DELETE------------------------------------------------------------
+//------------------------------------------TO DELETE OUTPUT DETAILS------------------------------------------------------------
 
 function deleteList(event){
     // console.log(event);
@@ -124,106 +128,106 @@ function deleteList(event){
           inputDetailArr.splice(index,1);
        }
     })
-    localStorage.setItem("detail",JSON.stringify(inputDetailArr));
+    localStorage.setItem("details",JSON.stringify(inputDetailArr));
     }
 
-//-------------------------------------------TO EDIT--------------------------------------------------------------
+//-------------------------------------------TO EDIT DETAILS--------------------------------------------------------------
 
 function editList(event){
     console.log(event)
-    listName=event.path[1].childNodes[0];
-    listCity=event.path[1].childNodes[2];
-    listPaan=event.path[1].childNodes[4];
-    listMail=event.path[1].childNodes[6];
-    inputName.value=listName.textContent;
-    inputCity.value=listCity.textContent;
-    inputPaan.value=listPaan.textContent;
-    inputMail.value=listMail.textContent;
-    addButton.value="Save";
+    listName = event.path[1].childNodes[0];
+    listCity = event.path[1].childNodes[2];
+    listPaan = event.path[1].childNodes[4];
+    listMail = event.path[1].childNodes[6];
+    inputName.value = listName.textContent;
+    inputCity.value = listCity.textContent;
+    inputPaan.value = listPaan.textContent;
+    inputMail.value = listMail.textContent;
+    addButton.value = "Save";
  }
 
-// -------------------------------------------TO SAVE UPDATED VALUE----------------------------------------------
+// -------------------------------------------TO SAVE THE UPDATED VALUE----------------------------------------------
 
 function SaveUpdate(event,nameText,cityText,mailText,paanText)
 {
-   listName.textContent=nameText;
-   listCity.textContent=cityText;
-   listMail.textContent=mailText;
-   listPaan.textContent=paanText;
-   addButton.value="Add";
+   listName.textContent = nameText;
+   listCity.textContent = cityText;
+   listMail.textContent = mailText;
+   listPaan.textContent = paanText;
+   addButton.value = "Add";
 
 // update the array
 inputDetailArr.forEach((ele,index)=>{
    if(listName.parentNode.parentNode.id==ele.id){
-      ele.name=nameText;
-      ele.city=cityText;
-      ele.paan=paanText;
-      ele.mail=mailText;
+      ele.name = nameText;
+      ele.city = cityText;
+      ele.paan = paanText;
+      ele.mail = mailText;
    }
 })
-localStorage.setItem("detail",JSON.stringify(inputDetailArr));
+localStorage.setItem("details",JSON.stringify(inputDetailArr));
 // console.log(inputDetailArr)
 reset();
 }
-//--------------------------------------------FILTER NAME--------------------------------------------------------
+//-------------------------------------------FILTER NAME(A TO Z & Z TO A)--------------------------------------------------------
 
 function filterName(event)
 {
    outputList.innerHTML = "";
    let temp;
-   if(event.target.value=="select"){
-    temp=inputDetailArr.sort((a,b)=>a.id-b.id);
-   }else if(event.target.value=="A_to_Z"){
-    temp=inputDetailArr.sort((a,b)=>a.name>b.name? 1:-1);
+   if(event.target.value == "select"){
+    temp = inputDetailArr.sort((a,b)=>a.id-b.id);
+   }else if(event.target.value == "A_to_Z"){
+    temp = inputDetailArr.sort((a,b)=>a.name>b.name? 1:-1);
    }
-   else if (event.target.value=="Z_to_A"){
-    temp=inputDetailArr.sort((a,b)=>a.name>b.name? -1:1);
+   else if (event.target.value == "Z_to_A"){
+    temp = inputDetailArr.sort((a,b)=>a.name>b.name? -1:1);
    }
 
    temp.forEach(ele=>{
        createListElement(ele);
    })
 }
-//------------------------------------------------FILTER PAAN---------------------------------------------------
+//------------------------------------------------FILTER BY PAAN NUMBER---------------------------------------------------
 
 function filterPaan(event){
     outputList.innerHTML = "";
 
-console.log(searchPaan.value)
- let searchInput=searchPaan.value.split("");
+// console.log(searchPaan.value);
 
- inputDetailArr.forEach((ele,index)=>{
-     let paanArr=ele.paan.split("");
-     if(paanArr[index]==searchInput[index])
-     {
-        createListElement(ele);
-     }
+let searchInput= inputDetailArr.filter((ele)=>{
+   if(ele.paan.includes(searchPaan.value)){
+      return ele;
+   }
  })
-searchButton.addEventListener("click",(event)=>{
-    outputList.innerHTML = "";
-     inputDetailArr.forEach((ele)=>{
-     if(searchPaan.value==ele.paan)
-     {
-        createListElement(ele);
-     }
-   //   else if(!(searchPaan.value==ele.paan)){
-   //   outputList.innerHTML=`<div class="error_paan error"> PAAN NOT FOUND </div`
- })
- 
-})
+if(searchInput.length!==0)
+{
+   searchInput.map((ele)=>{
+      createListElement(ele);
+   })
+}
+else{
+   outputList.innerHTML=`<div class="error_paan error"> PAAN NOT FOUND </div>`
+}
 
-
+// searchButton.addEventListener("click",(event)=>{
+//    outputList.innerHTML = "";
+//     inputDetailArr.forEach((ele)=>{
+//     if(searchPaan.value==ele.paan)
+//     {
+//        createListElement(ele);
+//     }
+// })
+// })
 crossIcon.addEventListener("click",(event)=>{
     outputList.innerHTML = "";
     inputDetailArr.forEach((ele)=>{
         createListElement(ele);
     })
+    searchPaan.value="";
 })
 
-
 }
-
-
 
 //---------------------------------------------------ADD EVENT LISTNERS-------------------------------------------
 
